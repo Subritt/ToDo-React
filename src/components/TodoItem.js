36afef76b9429252569902b07fe2d39c6373097a
Subritt <subrittburlakoti@gmail.com>
2020-09-01 21:30:04 +0545

    style with react

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 import PropType from 'prop-types';
 
 export class TodoItem extends Component {
+    getStyle = () => {
+        return {
+            background: '#f4f4f4',
+            padding: '10px',
+            borderBottom: '1px #ccc dotted',
+            textDecoration: this.props.todo.completed
+            ? 'line-through' : 'none'
+        }
+    }
     render() {
         return (
-            <div>
-                <h3>{ this.props.todo.title }</h3>
+            <div style={this.getStyle()}>
+                <p>
+                    <input type="checkbox" /> {' '}
+                    { this.props.todo.title }
+                </p>
             </div>
         )
     }
