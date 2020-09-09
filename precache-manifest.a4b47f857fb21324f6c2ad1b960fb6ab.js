661eb3197eb846c828bcae8cc3aab79bdbf9a2d5
Subritt <subrittburlakoti@gmail.com>
2020-09-09 22:37:01 +0545

    Updates

---
+++ precache-manifest.a4b47f857fb21324f6c2ad1b960fb6ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "764d8700007ac22c5f115c0f09d0bc35",
+    "url": "/ToDo-React/index.html"
+  },
+  {
+    "revision": "fd1df00275fdeeb9bafc",
+    "url": "/ToDo-React/static/css/main.a90de13b.chunk.css"
+  },
+  {
+    "revision": "be7af29d57fc2cea02d8",
+    "url": "/ToDo-React/static/js/2.8a2d4821.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ToDo-React/static/js/2.8a2d4821.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd1df00275fdeeb9bafc",
+    "url": "/ToDo-React/static/js/main.62437c96.chunk.js"
+  },
+  {
+    "revision": "947f5e964b1e2fe9b80a",
+    "url": "/ToDo-React/static/js/runtime-main.0b9a3756.js"
+  }
+]);
